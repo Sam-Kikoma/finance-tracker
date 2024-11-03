d61a0e0ee8246f519bd350167695395b0ce8977c
Sam Kikoma <kikomasamson@gmail.com>
2024-11-03 14:15:09 +0100

    Updated user routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,15 @@
 const userRouter = require("express").Router();
 const userController = require("../controllers/userController");
+const authorization = require("../middleware/authorization");
+const validInfo = require("../middleware/validInfo");
 
 //Route to create a new user
-userRouter.post("/register", userController.createUser);
+userRouter.post("/register", validInfo, userController.createUser);
 // Login
-userRouter.post("/login", userController.loginUser);
+userRouter.post("/login", validInfo, userController.loginUser);
+//Verification
+userRouter.get("/isVerified", authorization, userController.isVerified);
+//Dashboard
+userRouter.get("/dashboard", authorization, userController.userDashboard);
 
 module.exports = userRouter;
