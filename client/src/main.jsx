0e6fe085c9ecb20d17651359627e4daf3ca5d9b5
Sam Kikoma <kikomasamson@gmail.com>
2024-11-13 21:31:01 +0100

    React-router setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import Dashboard from "./components/Dashboard.jsx";
+import Register from "./components/Register.jsx";
+import Login from "./components/Login.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+		children: [
+			{
+				path: "login",
+				element: <Login />,
+			},
+			{
+				path: "register",
+				element: <Register />,
+			},
+			{
+				path: "dashboard",
+				element: <Dashboard />,
+			},
+		],
+	},
+]);
+
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>
+);
