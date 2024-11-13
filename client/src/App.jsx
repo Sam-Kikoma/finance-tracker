f5ae1599b373842fca192e8bf1114acd368e5839
Sam Kikoma <kikomasamson@gmail.com>
2024-11-13 21:28:54 +0100

    Setting up react route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { useState } from "react";
+import { Outlet, Navigate } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 const App = () => {
-	return <div>App</div>;
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
+	const setAuth = (boolean) => {
+		setIsAuthenticated(boolean);
+	};
+	console.log(isAuthenticated);
+
+	return (
+		<div>
+			{isAuthenticated ? <Navigate to="/dashboard" replace /> : <Navigate to="/login" replace />}
+			<Outlet context={{ setAuth, setIsAuthenticated }} />
+		</div>
+	);
 };
 
 export default App;
