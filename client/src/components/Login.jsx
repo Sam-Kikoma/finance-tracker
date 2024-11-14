aaef6544dfdfbeb0d924cbef0de8811e9e104b86
Sam Kikoma <kikomasamson@gmail.com>
2024-11-14 22:34:46 +0100

    Updated page flow

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,15 +1,24 @@
-import { useOutletContext } from "react-router-dom";
+import { useOutletContext, useNavigate } from "react-router-dom";
 
 const Login = () => {
 	const { setAuth } = useOutletContext();
 	const handleLogin = () => {
 		setAuth(true);
 	};
+	const navigate = useNavigate();
 	return (
 		<>
 			<h1>Login Page</h1>
-			<button onClick={handleLogin} className="border-2 border-black rounded-md">
+			<button onClick={handleLogin} className="border-1 border-black rounded-md">
 				Authenticate
+			</button>
+			<br />
+			<button
+				onClick={() => {
+					navigate("/register");
+				}}
+			>
+				Register
 			</button>
 		</>
 	);
