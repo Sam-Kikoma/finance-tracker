10e66d0f9d27b495e5624ddcc50f3732b115c24e
Sam Kikoma <kikomasamson@gmail.com>
2024-11-14 22:34:12 +0100

    Register component logic

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,8 +1,58 @@
 import { useState } from "react";
 
 const Register = () => {
-	return <div>Register</div>;
+	const [inputs, setInputs] = useState({
+		name: "",
+		email: "",
+		password: "",
+	});
+	const { name, email, password } = inputs;
+	const onChange = (e) => {
+		setInputs({ ...inputs, [e.target.name]: e.target.value });
+	};
+	const onSubmitForm = async (e) => {
+		e.preventDefault();
+		try {
+			// Continue from here
+			const res = await fetch("http://localhost");
+		} catch (err) {
+			console.error(err.message);
+		}
+	};
+	return (
+		<div className="flex items-center justify-center min-h-screen">
+			<div className="w-max h-auto flex flex-col gap-4 items-center justify-center">
+				<h1>Register</h1>
+				<form className="flex flex-col gap-4 items-center" onSubmit={onSubmitForm}>
+					<input
+						type="text"
+						name="name"
+						placeholder="Enter your name"
+						className="input input-bordered w-full max-w-xs"
+						value={name}
+						onChange={(e) => onChange(e)}
+					/>
+					<input
+						type="email"
+						name="email"
+						placeholder="Enter your email"
+						className="input input-bordered w-full max-w-xs"
+						value={email}
+						onChange={(e) => onChange(e)}
+					/>
+					<input
+						type="password"
+						name="password"
+						placeholder="Enter your password"
+						className="input input-bordered w-full max-w-xs"
+						value={password}
+						onChange={(e) => onChange(e)}
+					/>
+				</form>
+				<button>Submit</button>
+			</div>
+		</div>
+	);
 };
 
 export default Register;
-// Build the register component next
