55db4fcb83f8a1ae49c760ddd1cbd0145c3dc85f
Sam Kikoma <kikomasamson@gmail.com>
2024-11-17 21:32:03 +0100

    Initial login logic --done

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,26 +1,53 @@
-import { useOutletContext, useNavigate } from "react-router-dom";
+import { useOutletContext, Link } from "react-router-dom";
+import { useState } from "react";
 
 const Login = () => {
 	const { setAuth } = useOutletContext();
-	const handleLogin = () => {
-		setAuth(true);
+	const [inputs, setInputs] = useState({
+		email: "",
+		password: "",
+	});
+	const { email, password } = inputs;
+	const onChange = (e) => {
+		setInputs({ ...inputs, [e.target.name]: e.target.value });
 	};
-	const navigate = useNavigate();
+	const onSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			const body = { email, password };
+			const res = await fetch("http://localhost:3000/api/auth/login", {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify(body),
+			});
+			const parseRes = await res.json();
+			setAuth(true);
+			localStorage.setItem("token", parseRes.token);
+		} catch (err) {
+			console.error(err.message);
+		}
+	};
+
 	return (
-		<>
-			<h1>Login Page</h1>
-			<button onClick={handleLogin} className="border-1 border-black rounded-md">
-				Authenticate
-			</button>
-			<br />
-			<button
-				onClick={() => {
-					navigate("/register");
-				}}
-			>
-				Register
-			</button>
-		</>
+		<div className="flex items-center justify-center min-h-screen flex-col">
+			<div className="w-max h-auto">
+				<h1>Login</h1>
+			</div>
+			<div>
+				<form className="flex flex-col" onSubmit={onSubmit}>
+					<input type="email" name="email" placeholder="Enter your email" value={email} onChange={(e) => onChange(e)} />
+					<input
+						type="password"
+						name="password"
+						placeholder="Enter your password"
+						value={password}
+						onChange={(e) => onChange(e)}
+					/>
+					<button>Submit</button>
+				</form>
+				<Link to="/register">Register</Link>
+			</div>
+		</div>
 	);
 };
 
