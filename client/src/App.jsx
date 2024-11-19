14a71d779deb91d97c242b17bc609634a2c47122
Sam Kikoma <kikomasamson@gmail.com>
2024-11-19 21:55:22 +0100

    Refresh logic was added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Outlet, Navigate } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Login from "./components/Login";
 import Register from "./components/Register";
 
@@ -8,12 +10,27 @@ const App = () => {
 	const setAuth = (boolean) => {
 		setIsAuthenticated(boolean);
 	};
-	console.log(isAuthenticated);
+	const isAuth = async () => {
+		try {
+			const res = await fetch("http://localhost:3000/api/auth/isVerified", {
+				method: "GET",
+				headers: { token: localStorage.token },
+			});
+			const parseRes = await res.json();
+			parseRes === true ? setIsAuthenticated(true) : setIsAuthenticated(false);
+		} catch (err) {
+			console.error(err.message);
+		}
+	};
+	useEffect(() => {
+		isAuth();
+	}, []);
 
 	return (
 		<div>
 			{isAuthenticated ? <Navigate to="/dashboard" replace /> : <Navigate to="/login" replace />}
 			<Outlet context={{ setAuth, setIsAuthenticated }} />
+			<ToastContainer />
 		</div>
 	);
 };
