80551a9d2534a9b9f8668f02f016435ef7d9d947
Sam Kikoma <kikomasamson@gmail.com>
2024-11-19 21:56:00 +0100

    Initial dashboard logic

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,6 +1,38 @@
+import { useEffect, useState } from "react";
+import { useOutletContext } from "react-router-dom";
+import { toast } from "react-toastify";
+
 const Dashboard = () => {
-	// Start here
-	return <div>Dashboard</div>;
+	const [name, setName] = useState("");
+	const { setAuth } = useOutletContext();
+	const getName = async () => {
+		try {
+			const res = await fetch("http://localhost:3000/api/auth/dashboard", {
+				method: "GET",
+				headers: { token: localStorage.token },
+			});
+			const parseRes = await res.json();
+			setName(parseRes.user_name);
+		} catch (err) {
+			console.error(err.message);
+		}
+	};
+	const logOut = (e) => {
+		e.preventDefault();
+		localStorage.removeItem("token");
+		setAuth(false);
+		toast("Logged out successfully");
+	};
+	useEffect(() => {
+		getName();
+	}, []);
+	return (
+		<>
+			<h1>Dashboard</h1>
+			<p>Hello {name}</p>
+			<button onClick={(e) => logOut(e)}>Logout</button>
+		</>
+	);
 };
 
 export default Dashboard;
